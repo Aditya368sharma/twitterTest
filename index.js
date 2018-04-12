c20eef437981649371424158549d79fc872ee187
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-12 15:54:31 +0530

    Test

--- index.js
+++ index.js
@@ -18,27 +18,18 @@ var bot = new twit({
 })
 ///////////////////////////////////
 
-// Global variables needed to create the tweet
-var randomWord;
-var rhymingWord;
-var rhymingDef;
 
 // Callback chain
 var sendTweet = function(){
-  getWord(nowGetRhymingWord);
-  function nowGetRhymingWord(randomWord){
-    getRhymingWord(randomWord, nowGetRhymingDef);
-    function nowGetRhymingDef(rhymingWord){
-      getRhymingDef(rhymingWord, nowReturnTweet);
+
       function nowReturnTweet(definition){
-        var wordAndDef = capitalizeFirstLetter(randomWord) + ": " + rhymingDef;
+        var wordAndDef = "Hi"+ ": " + "adi";
         bot.post('statuses/update', { status: wordAndDef }, function(err, data, response) {
           console.log("Success!");
         });
       }
     }
-  }
-}
+
 
 // Send tweet every 28 minutes, and on start
 setInterval(function() {
@@ -46,40 +37,8 @@ setInterval(function() {
 }, 1700);
 sendTweet();
 
-app.get('/', (req, res) => res.send('Hello World!'));
 
-function getWord(callback){
-  randomWord = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=' + process.env.WORDNIK_KEY;
-  request(randomWord, function (error, response, body) {
-    randomWord = JSON.parse(body).word;
-    callback(randomWord);
-  });
-};
 
-function getRhymingWord(randomWord, callback){
-  rhymingWord = 'http://api.wordnik.com:80/v4/word.json/' + randomWord + '/relatedWords?useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=' + process.env.WORDNIK_KEY;
-    request(rhymingWord, function (error, response, body) {
-    try{
-      rhymingWord = JSON.parse(body)[0].words[0];
-      callback(rhymingWord);
-    }catch(err){
-      sendTweet(); // The word didn't rhyme with anything... restart
-    }
-  });
-};
-
-function getRhymingDef(rhymingWord, callback){
-  rhymingDef = 'http://api.wordnik.com:80/v4/word.json/' + rhymingWord + '/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=' + process.env.WORDNIK_KEY;
-  request(rhymingDef, function (error, response, body) {
-    rhymingDef = JSON.parse(body)[0].text;
-    callback(rhymingDef);
-  });
-};
-
-// Helper function for to capitalize the random word
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
 
 // Tells the Express app to listen to a port
 app.listen(app.get('port'), function() {
