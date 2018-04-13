64c9b66dfba18a8bd32bf2724c497d01c9fe0a7f
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-13 15:53:46 +0530

    Test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ stream.on('direct_message', function (eventMsg) {
     console.log(eventMsg)
     var params = {
       screen_name: 'jensonjms',
-      text: 'Hello World'
+      text: 'Hello World!'
     };
     bot.post('direct_messages/new', params, function(error, message, response) {
       if (error){
