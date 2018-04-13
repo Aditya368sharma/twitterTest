f5f9a62b8458d57f2172b31d494c0e2cb905f4ef
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-13 14:30:04 +0530

    Test

--- index.js
+++ index.js
@@ -1,63 +1,83 @@
-var express = require('express');
-var twit = require('twit');
-var request = require('request');
+// var express = require('express');
+// var twit = require('twit');
+// var request = require('request');
+//
+var Twitter = require('twitter');
 
-var app = express();
-app.set('port', process.env.PORT || 6000);
-
-if(process.env.TWITTER_CONSUMER_KEY == undefined){
-  require('./env.js');
-}
-
-var bot = new twit({
-  consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-  access_token:         process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
-///////////////////////////////////
-var stream = bot.stream('statuses/filter', { track: 'aditya_368' });
-
-stream.on('tweet', function (tweet) {
-    console.log("tweet.text",tweet.text);
-     replyTo(tweet, 'Greeting from Aditya!');
+var client = new Twitter({
+    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+    access_token:         process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-function replyTo(tweet, message) {
-	var text = '@' + tweet.user.screen_name + ' ' + message;
-	bot.post('statuses/update', { status: text, in_reply_to_status_id: tweet.user.id_str },
-	    function(err, data, response) {
-		console.log(data)
-	    }
-	);
-}
-
-////////////
-
-// Callback chain
-var sendTweet = function(){
-
-      function nowReturnTweet(definition){
-        var wordAndDef = "Hi"+ ": " + "adi";
-        bot.post('statuses/update', { status: wordAndDef }, function(err, data, response) {
-          console.log("Success!");
-        });
-      }
-    }
-
-
-// Send tweet every 28 minutes, and on start
-setInterval(function() {
-  sendTweet();
-}, 1700000);
-sendTweet();
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-
-
-// Tells the Express app to listen to a port
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var params = {
+  screen_name: 'nodejs',
+  text: 'Hello World'
+};
+client.post('direct_messages/new', params, function(error, message, response) {
+  if (!error) {
+    console.log(message);
+  }
 });
+
+
+// var app = express();
+// app.set('port', process.env.PORT || 6000);
+//
+// if(process.env.TWITTER_CONSUMER_KEY == undefined){
+//   require('./env.js');
+// }
+//
+// var bot = new twit({
+//   consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+//   access_token:         process.env.TWITTER_ACCESS_TOKEN,
+//   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+// })
+// ///////////////////////////////////
+// var stream = bot.stream('statuses/filter', { track: 'aditya_368' });
+//
+// stream.on('tweet', function (tweet) {
+//     console.log("tweet.text",tweet.text);
+//      replyTo(tweet, 'Greeting from Aditya!');
+// });
+//
+// function replyTo(tweet, message) {
+// 	var text = '@' + tweet.user.screen_name + ' ' + message;
+// 	bot.post('statuses/update', { status: text, in_reply_to_status_id: tweet.user.id_str },
+// 	    function(err, data, response) {
+// 		console.log(data)
+// 	    }
+// 	);
+// }
+//
+// ////////////
+//
+// // Callback chain
+// var sendTweet = function(){
+//
+//       function nowReturnTweet(definition){
+//         var wordAndDef = "Hi"+ ": " + "adi";
+//         bot.post('statuses/update', { status: wordAndDef }, function(err, data, response) {
+//           console.log("Success!");
+//         });
+//       }
+//     }
+//
+//
+// // Send tweet every 28 minutes, and on start
+// setInterval(function() {
+//   sendTweet();
+// }, 1700000);
+// sendTweet();
+//
+// app.get('/', (req, res) => res.send('Hello World!'));
+//
+//
+//
+// // Tells the Express app to listen to a port
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
