ff5051cfa8ae4cc6894c7b07446a0a51382bfaf5
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-13 14:05:12 +0530

    Test

--- index.js
+++ index.js
@@ -17,64 +17,63 @@ var bot = new twit({
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
 ///////////////////////////////////
-//var stream = bot.stream('statuses/filter', { track: 'aditya_368' });
+var stream = bot.stream('statuses/filter', { track: 'aditya_368' });
 app.get('/', (req, res) => res.send('Hello World!'));
 
 
-var stream = bot.stream('user');
+//var stream = bot.stream('user');
 
-// stream.on('tweet', function (tweet) {
-//     console.log("tweet.text",tweet.text);
-//      replyTo(tweet, 'Greeting from Aditya!');
-// });
-//
-// function replyTo(tweet, message) {
-// 	var text = '@' + tweet.user.screen_name + ' ' + message;
-// 	bot.post('statuses/update', { status: text, in_reply_to_status_id: tweet.user.id_str },
-// 	    function(err, data, response) {
-// 		console.log(data)
-// 	    }
-// 	);
-// }
+stream.on('tweet', function (tweet) {
+    console.log("tweet.text",tweet.text);
+     replyTo(tweet, 'Greeting from Aditya!');
+});
+
+function replyTo(tweet, message) {
+	var text = '@' + tweet.user.screen_name + ' ' + message;
+	bot.post('statuses/update', { status: text, in_reply_to_status_id: tweet.user.id_str },
+	    function(err, data, response) {
+		console.log(data)
+	    }
+	);
+}
 
 ////////////
 
-function replyToDirectMessage(){
-
-stream.on('direct_message', function (eventMsg) {
-var msg = eventMsg.direct_message.text;
-var screenName = eventMsg.direct_message.sender.screen_name;
-var userId = eventMsg.direct_message.sender.id;
-
-// reply object
-var replyTo = { user_id: userId,
-  text: "Thanks for your message :)",
-  screen_name: screenName };
-
-console.log(screenName + " says: " + msg );
-
-// avoid replying to yourself when the recipient is you
-if(screenName != eventMsg.direct_message.recipient_screen_name){
-
-  //post reply
-  bot.post("direct_messages/new",replyTo, function(err,data,response){
-          console.info(data);
-      });
-  }
-});
-}
-
+// function replyToDirectMessage(){
+//
+// stream.on('direct_message', function (eventMsg) {
+// var msg = eventMsg.direct_message.text;
+// var screenName = eventMsg.direct_message.sender.screen_name;
+// var userId = eventMsg.direct_message.sender.id;
+//
+// // reply object
+// var replyTo = { user_id: userId,
+//   text: "Thanks for your message :)",
+//   screen_name: screenName };
+//
+// console.log(screenName + " says: " + msg );
+//
+// // avoid replying to yourself when the recipient is you
+// if(screenName != eventMsg.direct_message.recipient_screen_name){
+//
+//   //post reply
+//   bot.post("direct_messages/new",replyTo, function(err,data,response){
+//           console.info(data);
+//       });
+//   }
+// });
+// }
 
 // Callback chain
-// var sendTweet = function(){
-//
-//       function nowReturnTweet(definition){
-//         var wordAndDef = "Hi"+ ": " + "adi";
-//         bot.post('statuses/update', { status: wordAndDef }, function(err, data, response) {
-//           console.log("Success!");
-//         });
-//       }
-//     }
+var sendTweet = function(){
+
+      function nowReturnTweet(definition){
+        var wordAndDef = "Hi"+ ": " + "adi";
+        bot.post('statuses/update', { status: wordAndDef }, function(err, data, response) {
+          console.log("Success!");
+        });
+      }
+    }
 
 
 // Send tweet every 28 minutes, and on start
